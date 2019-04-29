bc2daea38d1b266806e8fa3d9430e17b51da6a9a
Brighton Tipedze <brighton.hbsct@gmail.com>
2019-04-29 13:28:36 +0200

    Web Crawler v001 initial for rate.co.zw

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+import static controller.WebCrawler.zim_rate;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        zim_rate();
+    }
+}
